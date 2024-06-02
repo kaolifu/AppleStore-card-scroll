ce2421b416e2a478b6f0fe797c6f6af820e5261a
kaolifu <164891219+kaolifu@users.noreply.github.com>
2024-06-02 13:29:44 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -27,84 +27,84 @@
   //storeCard Data
   const storeCards = [
     {
-      bgImg: 'url(../img/store-card/store-card-40-ipad-pro-202405.jpeg)',
+      bgImg: 'url(./img/store-card/store-card-40-ipad-pro-202405.jpeg)',
       fontColor: 'fontwhite',
       itemType: 'IPAD PRO',
       title: 'ありえない一枚',
       price: '168,800円(税込)から'
     },
     {
-      bgImg: 'url(../img/store-card/store-card-40-iphone-15-pro-202309.jpeg)',
+      bgImg: 'url(./img/store-card/store-card-40-iphone-15-pro-202309.jpeg)',
       fontColor: 'fontwhite',
       itemType: 'IPHONE 15 PRO',
       title: 'Titanium',
       price: '159,800円(税込)から、または36回払いで\n4,438円/月から',
     },
     {
-      bgImg: 'url(../img/store-card/store-card-40-ipad-air-202405.jpeg)',
+      bgImg: 'url(./img/store-card/store-card-40-ipad-air-202405.jpeg)',
       fontColor: 'fontblack',
       itemType: 'IPAD AIR',
       title: 'Airに新風。',
       price: '98,800円(税込)から'
     },
     {
-      bgImg: 'url(../img/store-card/store-card-40-apple-pencil-202405.png)',
+      bgImg: 'url(./img/store-card/store-card-40-apple-pencil-202405.png)',
       fontColor: 'fontwhite',
       itemType: 'APPLE PENCIL PRO',
       title: 'もう、作らずにはいられない。',
       price: '21,800円(税込)'
     },
     {
-      bgImg: 'url(../img/store-card/store-card-13-iphone-nav-202309.png)',
+      bgImg: 'url(./img/store-card/store-card-13-iphone-nav-202309.png)',
       fontColor: 'fontblack',
       itemType: '月々の分割支払い',
       title: 'iPhone15を金利0%、36回払いで購入できます。通信キャリア契約付きなら、さらにお得に。',
       price: 'しかも、24か月目のアップグレードで残りの分割払いが不要に。'
     },
     {
-      bgImg: 'url(../img/store-card/store-card-40-watch-s9-202309_GEO_JP.jpeg)',
+      bgImg: 'url(./img/store-card/store-card-40-watch-s9-202309_GEO_JP.jpeg)',
       fontColor: 'fontwhite',
       itemType: 'APPLE WATCH SERIES 9',
       title: '頭脳、明るさ、パワーの未来形。',
       price: '59,800円(税込)から'
     },
     {
-      bgImg: 'url(../img/store-card/store-card-40-macbook-air-202402.jpeg)',
+      bgImg: 'url(./img/store-card/store-card-40-macbook-air-202402.jpeg)',
       fontColor: 'fontblack',
       itemType: 'MACBOOK AIR',
       title: 'フットワークまで軽くなる。',
       price: '148,800円(税込)から'
     },
     {
-      bgImg: 'url(../img/store-card/store-card-40-pride-202405.jpeg)',
+      bgImg: 'url(./img/store-card/store-card-40-pride-202405.jpeg)',
       fontColor: 'fontwhite',
       itemType: 'プライドエディションブレイデッドソロループ',
       title: 'プライドと輝こう。 ',
       price: '14,800円(税込)'
     },
     {
-      bgImg: 'url(../img/store-card/store-card-40-iphone-15-202309.jpeg)',
+      bgImg: 'url(./img/store-card/store-card-40-iphone-15-202309.jpeg)',
       fontColor: 'fontblack',
       itemType: 'IPHONE 15',
       title: 'うっとり ',
       price: '124,800円(税込)から、または36回払いで\n3,466円/月から'
     },
     {
-      bgImg: 'url(../img/store-card/store-card-40-watch-ultra-202309_GEO_JP.jpeg)',
+      bgImg: 'url(./img/store-card/store-card-40-watch-ultra-202309_GEO_JP.jpeg)',
       fontColor: 'fontblack',
       itemType: 'APPLE WATCH ULTRA 2',
       title: 'さらなる冒険を。',
       price: '128,800円(税込)から'
     },
     {
-      bgImg: 'url(../img/store-card/store-card-40-macbook-pro-202310.jpeg)',
+      bgImg: 'url(./img/store-card/store-card-40-macbook-pro-202310.jpeg)',
       fontColor: 'fontblack',
       itemType: 'MACBOOK PRO',
       title: 'パワーの美学。',
       price: '248,800円(税込)から'
     },
     {
-      bgImg: 'url(../img/store-card/store-card-40-watch-se-202403_GEO_JP.jpeg)',
+      bgImg: 'url(./img/store-card/store-card-40-watch-se-202403_GEO_JP.jpeg)',
       fontColor: 'fontblack',
       itemType: 'APPLE WATCH SE',
       title: 'こんなにできる。\nこんなに手軽。',
